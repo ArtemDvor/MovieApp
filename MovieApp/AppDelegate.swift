ee6ac211d5c2817cfb080aafa7a567a0dd0d31b9
Artem Dvornik <artem.dvornik@letmecode.org>
2020-10-02 10:52:49 +0300

    Initial commit.

---
+++ MovieApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MovieApp
+//
+//  Created by Artem Dvornic on 02.10.2020.
+//  Copyright © 2020 Letmecode. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    weak var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
